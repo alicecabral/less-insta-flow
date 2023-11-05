3dd702f3e69c3976acc7e3080a81a88cd8fe10fd
Alice Cabral <alicecamarques@gmail.com>
2023-11-05 19:50:18 -0300

    fix post count

--- script.js
+++ script.js
@@ -12,7 +12,7 @@ const executeCurrentTab = () => {
                 if (mutation.previousSibling instanceof HTMLElement && mutation.previousSibling.tagName === 'ARTICLE' && mutation.removedNodes.length == 0) {
                     // console.log(mutation);
                     postCount++; 
-                    if(postCount % 5 == 0) alert("Você visualizou " + postCount + " posts. Deseja continuar?");
+                    if(postCount % 10 == 0) alert("Você visualizou " + postCount + " posts. Deseja continuar?");
                 }
             });
         });
